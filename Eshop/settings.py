ce0bf523386805e442fd4277d288577d5cd3e3c8
mohd wajid <53942048+mabond@users.noreply.github.com>
2022-03-08 14:01:48 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -118,15 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/image/download/"
-MEDIA_ROOT = os.path.join(BASE_DIR, '/uploads/images')
+MEDIA_ROOT = BASE_DIR
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
